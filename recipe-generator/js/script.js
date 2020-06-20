bf119b5ff5133dae0edb06ba4dc8bb7c4bda45c2
Phil Billingsby <philbillingsby@gmail.com>
2020-06-20 17:23:50 -0400

    Initial commit

---
+++ recipe-generator/js/script.js
@@ -0,0 +1,29 @@
+function findRecipe() {
+  event.preventDefault()
+  let searchQuery = document.querySelector('input').value;
+  fetch('http://www.recipepuppy.com/api/?q=' + searchQuery)
+  .then(resp => resp.json())
+  .then(object => {
+    showRecipe(object.results);
+  })
+}
+function showRecipe(results) {
+  for (const elem in results) {
+    createCard(results[elem]);
+  }
+}
+
+function createCard(obj) {
+  const recipeDiv = document.getElementById('recipes');
+  let newDiv = document.createElement('div');
+  newDiv.classList.add('card', 'text-center');
+  if (obj.thumbnail) {
+    newDiv.innerHTML += `<img class="card-img-top thumbnail-image mx-auto" src="${obj.thumbnail}" alt="Card image cap">`
+  }
+  let rest = `<div class="card-body">
+    <h5 class="card-title">${obj.title}</h5>
+    <p class="card-text">${obj.ingredients}</p>
+    <a href="${obj.href}" target="_blank" class="btn btn-primary">Go somewhere</a>`
+  newDiv.innerHTML += rest;
+  recipeDiv.append(newDiv);
+}
