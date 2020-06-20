c1b1619bdab25c0756a19bf842671fc2e40dec7c
Phil Billingsby <philbillingsby@gmail.com>
2020-06-20 17:59:34 -0400

    Add numbered list items for ingredients

--- recipe-generator/js/script.js
+++ recipe-generator/js/script.js
@@ -9,21 +9,29 @@ function findRecipe() {
 }
 function showRecipe(results) {
   for (const elem in results) {
-    createCard(results[elem]);
+    createCard(results[elem], elem);
   }
 }
 
-function createCard(obj) {
+function createCard(obj, element) {
   const recipeDiv = document.getElementById('recipes');
   let newDiv = document.createElement('div');
+  
   newDiv.classList.add('card', 'text-center');
   if (obj.thumbnail) {
     newDiv.innerHTML += `<img class="card-img-top thumbnail-image mx-auto" src="${obj.thumbnail}" alt="Card image cap">`
   }
   let rest = `<div class="card-body">
-    <h5 class="card-title">${obj.title}</h5>
-    <p class="card-text">${obj.ingredients}</p>
+    <h4 class="card-title">${obj.title}</h4>
+    <h6><strong>Ingredients</strong></h6>
+    <ul class="card-text"></ul>
     <a href="${obj.href}" target="_blank" class="btn btn-primary">Full Recipe</a>`
   newDiv.innerHTML += rest;
   recipeDiv.append(newDiv);
+  let arr = obj.ingredients.split(", ");
+  for (const ingredient of arr) {
+    let li = document.createElement('li');
+    li.innerText = `${arr.indexOf(ingredient) + 1}. ${ingredient}`;
+    document.querySelectorAll('ul')[element].appendChild(li);
+  }
 }
