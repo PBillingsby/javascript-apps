8e0638e01f6ec8065ff3341a72fd28f084a6e643
Phil Billingsby <philbillingsby@gmail.com>
2020-06-29 20:09:13 -0400

    Add win/lose messages

--- trick-generator/script.js
+++ trick-generator/script.js
@@ -1,8 +1,9 @@
-
+let attemptCount = 0;
 let gameCount = 0;
 let flipTricks = ["Ollie", "Kickflip", "Heelflip", "Shuv-It", "One Footer"];
 let spins = ["180", "360"];
 let stance = ["Frontside", "Backside", "Nollie", "Fakie"];
+let difficulty = 0
 function randomFunc(arr) {
   return arr[Math.floor(Math.random()*arr.length)]
 } 
@@ -22,31 +23,47 @@ function findTricks(value) {
       break;
     }
   let youtubeString = trickDiv.innerText.slice(5).replace('!', "").split(" ").join("+");
-  trickDiv.innerHTML += `<select onchange="landedTrick()" id="landed"><option default disabled>Landed it?</option><option>Yes</opton><option>No</option></select><p>Find trick tip <a href="https://www.youtube.com/results?search_query=${youtubeString}+trick+tip+skateboard" target="_blank" class="skateboard">here</a></p>`
+  trickDiv.innerHTML += `<label>Landed? </label><select onchange="landedTrick()" id="landed"><option selected="true" disabled>-</option><option>Yes</opton><option>No</option></select><p>Find trick tip <a href="https://www.youtube.com/results?search_query=${youtubeString}+trick+tip+skateboard" target="_blank" class="skateboard">here</a></p>`
 }
 
 function landedTrick() {
   let skate = ["S", "K", "A", "T", "E"];
   let select = document.getElementById('landed')
+  const result = document.getElementById('result')
   if (select.value === "No") {
     document.getElementById('letters').innerHTML += `<h1 class="d-inline skate-letter">${skate[gameCount]}</h1>`
     select["selectedIndex"] = 0;
     gameCount++;
+    findTricks(difficulty)
   }
   else {
+    attemptCount++
+    findTricks(difficulty)
   }
   if (gameCount === 5) {
-    document.getElementById('loss').style.display = "block";
-    setTimeout(location.reload(), 15000)
+    result.style.display = "block";
+    result.innerHTML += `<h2>You Lost!</h2><input type="submit" value="Play Again" onclick="refresh()">`;
+
   }
+  else if (attemptCount === 10) {
+    result.style.display = "block";
+    result.innerHTML += `<h2>You Win!</h2><input type="submit" value="Play Again" onclick="refresh()">`;
+
+  }
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('form-show').style.display = "none";
 })
 function showForm() {
-  event.preventDefault()
-  findTricks(document.getElementById('select-box').selectedIndex);
+  // event.preventDefault();
+  difficulty = document.getElementById('select-box').selectedIndex;
+  findTricks(difficulty);
   document.getElementById('play-game').style.display = "none";
   document.getElementById('form-show').style.display = "block";
+}
+
+function refresh() {
+  location.reload()
 }
